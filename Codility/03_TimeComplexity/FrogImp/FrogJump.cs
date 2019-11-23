9dd2e768de5536dbda91fcf53c61909e46d8697b
marshmallow.overlord <marshmallow.overlord@gmail.com>
2019-11-23 12:15:41 +0800

    Add codility solutions

---
+++ Codility/03_TimeComplexity/FrogImp/FrogJump.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingPractice
+{
+    class FrogJump
+    {
+        public int solution(int X, int Y, int D)
+        {
+            int distance = Y - X;
+            return (int)Math.Ceiling((double)distance / D);
+        }
+
+    }
+}
---
+++ Codility/04_CountingElements/MissingInteger/MissingInteger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingPractice
+{
+    class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            HashSet<int> set = new HashSet<int>(A);
+
+            int num = 1;
+            int counter = 1;
+            bool found = false;
+            while (!found)
+            {
+                if (!set.Contains(counter))
+                {
+                    found = true;
+                    num = counter;
+                    break;
+                }
+                counter++;
+            }
+
+            return num;
+        }
+
+    }
+}
