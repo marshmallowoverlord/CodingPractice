848e82bf17b243073f903544bbcf0dde6ca48fe9
marshmallow.overlord <marshmallow.overlord@gmail.com>
2019-11-23 11:24:40 +0800

    Add codility solutions

---
+++ Codility/02_Arrays/CyclicRotation/CyclicRotation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodingPractice
+{
+    class CyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            int[] result = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                result[(i + K) % A.Length] = A[i];
+            }
+
+            return result;
+        }
+
+    }
+}
