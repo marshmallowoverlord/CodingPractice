7b8cc01f78131277f4fc1ad4825bee538339a5b6
marshmallow.overlord <marshmallow.overlord@gmail.com>
2019-12-07 01:39:27 +0800

    GraffitiOntheFence

---
+++ CodingPractice/CodinGame/Easy/GraffitiOnTheFence/GraffitiOntheFence.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodingPractice.CodinGame.Easy.GraffitiOnTheFence
+{
+    class GraffitiOntheFence
+    {
+        static void Main(string[] args)
+        {
+            int L = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            List<Node> unpainted = new List<Node>();
+
+            List<Node> unsections = new List<Node>();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int st = int.Parse(inputs[0]);
+                int ed = int.Parse(inputs[1]);
+
+                unsections.Add(new Node(st, ed));
+            }
+
+            List<Node> sections = unsections.OrderBy(x => x.Start).ToList();
+
+            int prevEnd = 0;
+            int currentStart = sections[0].Start;
+            int currentEnd = sections[0].End;
+
+            if (currentStart > 0)
+            {
+                unpainted.Add(new Node(prevEnd, currentStart));
+            }
+
+            for (int i = 1; i < N; i++)
+            {
+                //Console.Error.WriteLine("== " + i + " : " + currentStart + " " + currentEnd);
+                if (sections[i].Start <= currentEnd)
+                {
+                    currentEnd = Math.Max(currentEnd, sections[i].End);
+                }
+                else
+                {
+                    prevEnd = currentEnd;
+                    currentStart = sections[i].Start;
+                    currentEnd = sections[i].End;
+                    unpainted.Add(new Node(prevEnd, currentStart));
+                }
+            }
+
+            if (currentEnd < L)
+            {
+                unpainted.Add(new Node(currentEnd, L));
+            }
+
+            bool painted = false;
+            foreach (Node n in unpainted)
+            {
+                if (unpainted[0].Start != unpainted[0].End)
+                {
+                    Console.WriteLine(n.Start + " " + n.End);
+                    painted = true;
+                }
+            }
+
+            if (unpainted.Count() == 0 || !painted)
+            {
+                Console.WriteLine("All painted");
+            }
+        }
+
+        public class Node
+        {
+            public int Start;
+            public int End;
+
+            public Node(
+                int start,
+                int end
+            )
+            {
+                Start = start;
+                End = end;
+            }
+        }
+    }
+}
+}
--- CodingPractice/CodinGame/Easy/UnitFractions/UnitFractions.cs
+++ CodingPractice/CodinGame/Easy/UnitFractions/UnitFractions.cs
@@ -49,22 +49,5 @@ namespace CodingPractice.CodinGame.Easy.UnitFractions
                 Y = y;
             }
         }
-
-        public static int LCM(int a, int b)
-        {
-            return (a * b) / GCD(a, b);
-        }
-
-        public static int GCD(int a, int b)
-        {
-            if (b == 0)
-            {
-                return a;
-            }
-            else
-            {
-                return GCD(b, a % b);
-            }
-        }
     }
 }
